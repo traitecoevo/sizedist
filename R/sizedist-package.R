7404bd76399fc4f6026fea010e65c35e19a67dfb
Fonti Kar <f.kar@unsw.edu.au>
2021-08-23 13:49:54 +1000

    Added basic R package skeleton

---
+++ R/sizedist-package.R
@@ -0,0 +1,16 @@
+#' The 'sizedist' package.
+#'
+#' @description A DESCRIPTION OF THE PACKAGE
+#'
+#' @docType package
+#' @name sizedist-package
+#' @aliases sizedist
+#' @useDynLib sizedist, .registration = TRUE
+#' @import methods
+#' @import Rcpp
+#' @importFrom rstan sampling
+#'
+#' @references
+#' Stan Development Team (2020). RStan: the R interface to Stan. R package version 2.21.2. https://mc-stan.org
+#'
+NULL
