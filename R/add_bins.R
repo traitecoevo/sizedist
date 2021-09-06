7b8ae5f29e004cc8c86d0a8a1e7f9124ec365016
Fonti Kar <f.kar@unsw.edu.au>
2021-09-06 15:00:22 +1000

    generalised add_bins function and added glue

--- R/add_bins.R
+++ R/add_bins.R
@@ -3,36 +3,39 @@
 #' add_bins() adds size and/or age bins to the dataset.
 #'
 #' @param data A data frame, data frame extension (e.g. tibble) containing `age` and `size` variable as columns
-#' @param age_var Name of age variable, using tidy evaluation with data masking
-#' @param size_var Name of size variable, using tidy evaluation with data masking
-#' @param bin_width_age The bin width for age, usually the smallest resolution of resampling e.g. 1 day
-#' @param bin_width_size The bin width for size, usually the smallest resolution of measurement e.g 0.1 mm
-#' @return a data frame with `age_bin` and `size_bin` variables added
+#' @param var Name of variable, using tidy evaluation with data masking
+#' @param bin_width The bin width for variable, usually the smallest resolution of resampling or measurement
+#' @return a data frame with binned variable added with suffix `_bin`
 #' @export
 #' @seealso simulate_population()
 #' @examples
 #' \dontrun{
-#' fishlarvae %>% add_bins()
 #'
-#' data %>% add_bins(age_var = days_since_birth, size_var = length)
+#' Loblolly %>% add_bins(var = height, bin_width = 0.5)
 #' }
 #'
 add_bins <- function(data,
-                     age_var = age,
-                     size_var = size_sampled,
-                     bin_width_age = 1,
-                     bin_width_size = 0.1) {
+                     var,
+                     bin_width) {
 
   round_by_bin <- function(x, bin_width) {
     round(x/bin_width, 0)*bin_width
   }
 
-  out <- data %>%
+  # Rounding data value to specific bin
+  tmp <- data %>%
     dplyr::mutate(
-      age_bin = round_by_bin({{age_var}}, bin_width_age),
-      size_bin = round_by_bin({{size_var}}, bin_width_size)
+     binned_var = round_by_bin({{var}}, bin_width)
     )
 
-  out
+  # Suffix to append to end of variable name
+  suffix <-"_bin"
+
+  # Renaming the variable using supplied variable name and suffix
+  out <- tmp %>%
+    dplyr::rename("{{var}}{suffix}" := binned_var)
+
+  #Return as tibble
+  dplyr::tibble(out)
 
 }
