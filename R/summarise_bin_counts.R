b74abc4459918bb9273cd579ffa0ef1e2ce786e5
Fonti Kar <f.kar@unsw.edu.au>
2021-09-22 11:44:33 +1000

    Updated tests for _bins and updated summarise_

--- R/summarise_bin_counts.R
+++ R/summarise_bin_counts.R
@@ -1,17 +1,27 @@
-
-
+#' Summarise counts for each bin interval
+#'
+#' @param data A data frame or tibble
+#' @param bin_var Numeric variable that bin intervals and counts are computed
+#' @param bin_width The bin width for `age`bin_var`, usually the smallest resolution of re-sampling
+#'
+#' @return A tibble containing `bin_var`, lower and upper bounds and counts for each bin
+#' @export
+#'
+#' @examples
+#' Loblolly %>% summarise_bin_counts(height, bin_width = 0.5)
+#'
 summarise_bin_counts <- function(data, bin_var, bin_width) {
 
   #Add bin variable to data
   data <- data %>% add_bins({{bin_var}}, bin_width)
 
- bin_var <- enquo(bin_var)
+ bin_var <- rlang::enquo(bin_var)
 
  #Get counts
  bin_counts <- data %>%
-   group_by(binned_var) %>%
-   summarise(counts = n()) %>%
-   ungroup()
+   dplyr::group_by(binned_var) %>%
+   dplyr::summarise(counts = n()) %>%
+   dplyr::ungroup()
 
   #Create all possible bins
   create_all_bins <- function(vec, bin_width){
@@ -28,21 +38,63 @@ summarise_bin_counts <- function(data, bin_var, bin_width) {
 
   #Creating new df with all bins
   tmp <- dplyr::tibble(
-    binned_var = data %>% pull(binned_var) %>% create_all_bins(bin_width),
+    binned_var = data %>% dplyr::pull(binned_var) %>% create_all_bins(bin_width),
     !!bin_nm := binned_var,
     !!lb_name := binned_var - 0.5 * bin_width,
     !!ub_name := binned_var + 0.5 * bin_width)
 
   #Get the bin counts and then joining to data
-  tmp %>% left_join(by = "binned_var", bin_counts) %>%
+  out <- tmp %>% dplyr::left_join(by = "binned_var", bin_counts) %>%
     tidyr::replace_na(list(counts = 0)) %>%
     dplyr::select(-binned_var)
 
+  #Correct the lower bounds of the first and last bins
+  out[1,2] <- out[1,1]
+  out[nrow(out),2] <-out[nrow(out), 1]
+
+  out
 }
 
-# #data <- simulate_population() %>% add_bins(size_sampled, 0.1)
-data %>% summarise_bin_counts(size, 0.1)
-summarise_bin_counts(Loblolly, age, 10)
+#' Creates bins
+#'
+#' add_bins() adds size and/or age bins to the dataset.
+#'
+#' @param data A data frame, data frame extension (e.g. tibble) containing `age` and `size` variable as columns
+#' @param var Name of variable, using tidy evaluation with data masking
+#' @param bin_width The bin width for variable, usually the smallest resolution of resampling or measurement
+#' @return a data frame with binned variable added with suffix `_bin`
+#' @export
+#' @importFrom rlang :=
+#' @seealso simulate_population()
+#' @examples
+#' \dontrun{
+#'
+#' Loblolly %>% add_bins(var = height, bin_width = 0.5)
+#' }
+#'
+add_bins <- function(data,
+                     var,
+                     bin_width) {
 
-data$size %>% unique()
+  round_by_bin <- function(x, bin_width) {
+    round(x/bin_width, 0)*bin_width
+  }
+
+  if(missing(var)){
+    stop("`var` is missing! Which variable do you want to add bins to?")
+  }
+
+  if(missing(bin_width)){
+    stop("`bin_width` must be supplied! This is usually the smallest measurement resolution for `var`")
+  }
+
+  # Rounding data value to specific bin
+  data %>%
+    dplyr::tibble() %>% #Become tibble
+    dplyr::mutate(
+      binned_var = round_by_bin({{var}}, bin_width)
+    )
+
+}
+
 
--- tests/testthat/test-add_bins.R
+++ tests/testthat/test-add_bins.R
@@ -1,14 +1,14 @@
 #Example dataset
 bin_width = 0.1
 example_1 <- Loblolly %>%
-  add_bins(var = height, bin_width = bin_width)
+  summarise_bin_counts(height, bin_width)
 
 test_that("Returned output is correct", {
   expect_visible(example_1)
   expect_named(example_1)
-  expect_true(ncol(Loblolly) < ncol(example_1))
-  expect_equal(ncol(example_1), stringr::str_which(names(example_1), "_bin"))
-  expect_equal(example_1[1,"height_bin"],
+  expect_equal(ncol(example_1), 4)
+  expect_equal(example_1[,2], example_1[,stringr::str_which(names(example_1), "_lower")])
+  expect_equal(example_1[1,"height"],
                (round(example_1[1,"height"] / bin_width, 0) * bin_width),
                ignore_attr = TRUE)
 })
