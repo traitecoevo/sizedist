529a89d43019dde01f0abadf09fe07290b2620b3
Fonti Kar <f.kar@unsw.edu.au>
2021-11-02 15:54:44 +1100

    Changed naming of models

--- R/fit_sizedist.R
+++ R/fit_sizedist.R
@@ -8,9 +8,9 @@
 fit_sizedist <- function(data, ...) {
   out <-
     switch(data$model,
-           model1 = rstan::sampling(stanmodels$mortality_age, data = data, ...),
-           model2 = rstan::sampling(stanmodels$mortality_size_known_g, data = data, ...),
-           model3 = rstan::sampling(stanmodels$mortality_size_growth, data = data, ...)
+           model1_a = rstan::sampling(stanmodels$mortality_age, data = data, ...),
+           model1_b = rstan::sampling(stanmodels$mortality_size_known_g, data = data, ...),
+           model1_c = rstan::sampling(stanmodels$mortality_size_growth, data = data, ...)
   )
 
   return(out)
