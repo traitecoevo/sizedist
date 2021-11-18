050e86343c9075fbc8ac627135f58e2c33453cd3
Fonti Kar <f.kar@unsw.edu.au>
2021-11-18 12:20:11 +1100

    Added documentation for default_priors

--- R/default_priors.R
+++ R/default_priors.R
@@ -1,37 +1,51 @@
+#' Load default priors for a specific model
+#'
+#' @param model name of model
+#' @param pars pars list object you want to join priors to
+#'
+#' @return list object with original pars with priors joined at the end
+#' @export
+#'
+#' @examples
+#' pars <- default_pars("model1")
+#' pars_new <- default_priors("model1a", pars)
+
 default_priors <- function(model = "model1a", pars){
   #Switch for different models
-  switch(model,
-        model1a = default_priors_model1a(),
-        model1b = default_priors_model1b(),
-        model1c = default_priors_model1c()
-        )
-}
-
-default_priors_model1a <- function(){
- priors <- list(
-    Z_sd = 10
+  priors <- switch(model,
+                   model1a = default_priors_model1a(),
+                   model1b = default_priors_model1b(),
+                   model1c = default_priors_model1c()
   )
 
-  list(priors = priors)
+  c(pars,
+    priors)
+}
+
+#'@rdname default_priors
+
+default_priors_model1a <- function(){
+  list(priors = list(Z_sd = 10)
+  )
   }
 
+#'@rdname default_priors
 default_priors_model1b <- function(){
-  priors <- list(
+  list(priors = list(
     Z_sd = 10,
     g_sd = 0.0001,
     R_sd = 100)
-
-  list(priors = priors)
+  )
 }
 
+#'@rdname default_priors
 default_priors_model1c <- function(){
-  priors <- list(
+  list(priors = list(
     Z_sd = 10,
     g_sd = 0.0001,
     R_sd = 100,
     sigma_size_sd = 2.5)
-
-  list(priors = priors)
+    )
 }
 
 
