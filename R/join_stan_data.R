6edc61a048aa790c53a1456c089bb2a311784b55
Fonti Kar <f.kar@unsw.edu.au>
2021-11-23 11:19:38 +1100

    Moved compass all new models to hier branch, Revertting back to "Minor typos"
    This reverts commit a0d288d7d77f166a6be8d303103696585c259786.

--- R/join_stan_data.R
+++ R/join_stan_data.R
@@ -1,4 +1,6 @@
-#' Merge bin counts and growth data for model1c
+#Function to return pars that are used to fit the model, switch based on pars$model (model prefix e.g model1)
+
+#' Merge bin counts and growth data for model3
 #'
 #' @param counts_list Stan-friendly count data created using `summarise_by_counts`
 #' @param growth_list Stan-friendly growth data created using `compose_growth_data`
