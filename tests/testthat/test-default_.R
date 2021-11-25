f6f2fd3e81cdaa4ca55d97ff53e01b6d239a0c2b
Fonti Kar <f.kar@unsw.edu.au>
2021-11-25 12:35:49 +1100

    Updated test coverage badge

--- tests/testthat/test-default_.R
+++ tests/testthat/test-default_.R
@@ -1,9 +1,22 @@
+# Getting pars and priors together
+
+new_pars <-
+  default_pars("model1") %>%
+  purrr::list_modify(model = "model1c") %>%
+  default_priors()
+
+######################################
 
 test_that("Output in correct format", {
   expect_visible(default_pars("model1"))
   expect_length(default_pars("model1"), 2)
   expect_named(default_pars("model1"))
   expect_type(default_pars("model1"), "list")
+
+  expect_visible(new_pars)
+  expect_length(new_pars, 3)
+  expect_named(new_pars)
+  expect_type(new_pars, "list")
 })
 
 
